c85f04b589aa27b8f76fc95c8f67ef547f132d11
wayalbhushan <wayalbhushan7144@gmail.com>
2025-08-25 17:28:49 +0530

    Update api.js

--- vaultx-client/src/utils/api.js
+++ vaultx-client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://vault-x-backend.vercel.app/api",
 });
 
 export const setAuthToken = (token) => {
